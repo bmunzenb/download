a9ad7d7233b0ae31e8702884a2a8d358e5d88130
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2020-01-10 14:26:59 -0500

    add target factories

--- app/src/main/kotlin/com/munzenberger/download/app/Main.kt
+++ app/src/main/kotlin/com/munzenberger/download/app/Main.kt
@@ -1,18 +1,14 @@
 package com.munzenberger.download.app
 
 import com.munzenberger.download.core.FileTargetFactory
-import com.munzenberger.download.core.RangeParamIterator
-import com.munzenberger.download.core.TemplateURLQueue
 import com.munzenberger.download.core.URLQueue
 import com.munzenberger.download.core.download
 
 fun main(args: Array<String>) {
 
-    val params = RangeParamIterator(1, 100)
+    val queue = URLQueue.of("http://example.com")
 
-    val queue = TemplateURLQueue("http://example.com/%d", params)
-
-    val targetFactory = FileTargetFactory("/Users/bmunzenb/Desktop/poop.jpg")
+    val targetFactory = FileTargetFactory("/Users/example/Desktop")
 
     download(queue, targetFactory)
 }
--- core/src/main/kotlin/com/munzenberger/download/core/TargetFactory.kt
+++ core/src/main/kotlin/com/munzenberger/download/core/TargetFactory.kt
@@ -1,6 +1,7 @@
 package com.munzenberger.download.core
 
 import java.io.File
+import java.io.IOException
 import java.net.URL
 
 interface TargetFactory {
@@ -10,4 +11,60 @@ interface TargetFactory {
 class FileTargetFactory(private val file: File, private val append: Boolean = false) : TargetFactory {
     constructor(path: String, append: Boolean = false) : this(File(path), append)
     override fun targetFor(source: URL)  = FileTarget(file, append)
+}
+
+/**
+ * Generates a target that writes to file named by the URL path in the specified base directory.
+ */
+class FlatPathFileTargetFactory(private val baseDir: File) : TargetFactory {
+    constructor(baseDir: String) : this(File(baseDir))
+    override fun targetFor(source: URL): Target {
+
+        if (!baseDir.exists() && !baseDir.mkdirs()) {
+            throw IOException("Could not mkdirs for baseDir $baseDir")
+        }
+
+        val filename = StringBuilder(source.host)
+        val parts = source.path.split('/')
+
+        parts.forEach {
+            filename.append('_').append(it)
+        }
+
+        val path = "${baseDir.path}${File.separator}$filename"
+
+        return FileTarget(path)
+    }
+}
+
+/**
+ * Generates a target that writes a file to a directory structure based on the source URL.
+ */
+class URLPathFileTargetFactory(private val baseDir: File) : TargetFactory {
+    constructor(baseDir: String) : this(File(baseDir))
+    override fun targetFor(source: URL): Target {
+
+        val path = StringBuilder(baseDir.path)
+                .append(File.separator)
+                .append(source.host)
+
+        val parts = source.path.split('/')
+
+        // the last part is the filename
+        parts.take(parts.size-1).forEach {
+            path.append(File.separator).append(it)
+        }
+
+        File(path.toString()).apply {
+            if (!exists() && !mkdirs()) {
+                throw IOException("Could not mkdirs for $path")
+            }
+        }
+
+        parts.last().apply {
+            path.append(File.separator).append(this)
+        }
+
+        return FileTarget(path.toString())
+    }
 }
